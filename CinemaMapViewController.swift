a9a8e9727e4540f124f563454a4765beca9ee0d3
imashrimp <kwonhs8287@gmail.com>
2023-08-24 00:27:23 +0900

    location button branch processing

--- CinemaMapViewController.swift
+++ CinemaMapViewController.swift
@@ -15,6 +15,8 @@ class CinemaMapViewController: UIViewController {
     
     let locationManager = CLLocationManager()
     let mapView = MKMapView()
+    var currentAuthorizationStatus: Int32?
+    var currentLocation: CLLocationCoordinate2D?
     
     let filterButton = {
         let button = UIButton()
@@ -38,37 +40,23 @@ class CinemaMapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(mapView)
-        view.addSubview(locationButton)
+
+        configureVC()
         makeConstraints()
         
-        filterButton.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: filterButton)
-        locationButton.addTarget(self, action: #selector(locationButtonTapped), for: .touchUpInside)
-        
-        locationManager.delegate = self
-        
         setAnnotation(id: .all)
-        
         checkDeviceLocationAuthorization()
-        
-        view.backgroundColor = .white
-        self.navigationController?.navigationBar.tintColor = .darkGray
     }
     
-    //MARK: - 매개변수 사용해서 분기처리하기
-    func setAnnotation(id: CinemaType) {
-        mapView.removeAnnotations(mapView.annotations)
-        
-        let annotationList = filteringCinemaTapped(id: id)
-        
-        mapView.addAnnotations(annotationList)
-    }
-    
-
     @objc func locationButtonTapped() {
-        locationAuthorizeAlert()
+        
+        switch currentAuthorizationStatus {
+        case 3, 4:
+            guard let myCurrentLocation = currentLocation else { return }
+            setRegionAndAnnotation(center: myCurrentLocation)
+        default:
+            locationAuthorizeAlert()
+        }
     }
     
     @objc func filterButtonTapped() {
@@ -84,17 +72,12 @@ class CinemaMapViewController: UIViewController {
         }
     }
     
-    func makeConstraints() {
+    func setAnnotation(id: CinemaType) {
+        mapView.removeAnnotations(mapView.annotations)
         
-        mapView.snp.makeConstraints { make in
-            make.edges.equalTo(view.safeAreaLayoutGuide)
-        }
+        let annotationList = filteringCinemaTapped(id: id)
         
-        locationButton.snp.makeConstraints { make in
-            make.size.equalTo(40)
-            make.leading.equalTo(mapView.snp.leading).offset(20)
-            make.bottom.equalTo(mapView.snp.bottom).inset(40)
-        }
+        mapView.addAnnotations(annotationList)
     }
     
     func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
@@ -109,6 +92,54 @@ class CinemaMapViewController: UIViewController {
     }
 }
 
+//MARK: - Location Authorization
+extension CinemaMapViewController {
+    func checkDeviceLocationAuthorization() {
+        
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                
+                let authorization: CLAuthorizationStatus
+                
+                if #available(iOS 14.0, *) {
+                    authorization = self.locationManager.authorizationStatus
+                } else {
+                    //MARK: - 14.0 아래의 버전에서는 무슨 권한의 어떤 상태를 가져온다는 거지?
+                    authorization = CLLocationManager.authorizationStatus()
+                }
+                DispatchQueue.main.async {
+                    self.checkCurrentLocationAuthorization(status: authorization)
+                    self.currentAuthorizationStatus = authorization.rawValue
+                }
+            } else {
+                print("위치 서비스가 꺼져있어서 위치 권한 요청을 할 수 없습니다.")
+            }
+        }
+    }
+    
+    func checkCurrentLocationAuthorization(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+            locationManager.startUpdatingLocation()
+        case .authorizedAlways:
+            print("authorizedAlways")
+            locationManager.startUpdatingLocation()
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        case .authorized:
+            print("authorized")
+        @unknown default:
+            print("default")
+        }
+    }
+}
+
 extension CinemaMapViewController: CLLocationManagerDelegate {
     
     //사용자 위치 성공적으로 가져올 때마다 호출, kCLLocationAccuracyBest가 판단해서 호출함
@@ -116,6 +147,7 @@ extension CinemaMapViewController: CLLocationManagerDelegate {
         
         if let coordinate = locations.last?.coordinate {
             print(coordinate)
+            currentLocation = coordinate
             //이게 현재 위치를 디바이스 위치로 설정
             setRegionAndAnnotation(center: coordinate)
         }
@@ -141,54 +173,33 @@ extension CinemaMapViewController: CLLocationManagerDelegate {
     }
 }
 
-//MARK: - Location Authorization
+//MARK: - ViewControllerConfigure
 extension CinemaMapViewController {
-    func checkDeviceLocationAuthorization() {
+    func configureVC() {
+        view.backgroundColor = .white
+        self.navigationController?.navigationBar.tintColor = .darkGray
         
-        DispatchQueue.global().async {
-            if CLLocationManager.locationServicesEnabled() {
-                
-                let authorization: CLAuthorizationStatus
-                
-                if #available(iOS 14.0, *) {
-                    authorization = self.locationManager.authorizationStatus
-                } else {
-                    //MARK: - 14.0 아래의 버전에서는 무슨 권한의 어떤 상태를 가져온다는 거지?
-                    authorization = CLLocationManager.authorizationStatus()
-                }
-                print(authorization)
-                //MARK: - 이거를 왜 dispatch queue.main.async에서 해야하는거지?
-                DispatchQueue.main.async {
-                    self.checkCurrentLocationAuthorization(status: authorization)
-                }
-            } else {
-                print("위치 서비스가 꺼져있어서 위치 권한 요청을 할 수 없습니다.")
-            }
-        }
+        view.addSubview(mapView)
+        view.addSubview(locationButton)
         
+        
+        filterButton.addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: filterButton)
+        locationButton.addTarget(self, action: #selector(locationButtonTapped), for: .touchUpInside)
+        
+        locationManager.delegate = self
     }
     
-    
-    func checkCurrentLocationAuthorization(status: CLAuthorizationStatus) {
-        switch status {
-        case .notDetermined:
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestAlwaysAuthorization()
-        case .restricted:
-            print("restricted")
-        case .denied:
-            print("denied")
-            locationManager.startUpdatingLocation()
-        case .authorizedAlways:
-            //MARK: - 여기서도 위치 업데이트 해줘야하는거 아닌가? whenInUse에서만 업데이트 하면 여기도 처리가 알아서 되는건지 찾아보자
-            print("authorizedAlways")
-        case .authorizedWhenInUse:
-            //MARK: - 아래의 메서드가 딜리게이트의 didUpdate 메서드를 호출하는지 확인
-            locationManager.startUpdatingLocation()
-        case .authorized:
-            print("authorized")
-        @unknown default:
-            print("default")
+    func makeConstraints() {
+        
+        mapView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        locationButton.snp.makeConstraints { make in
+            make.size.equalTo(40)
+            make.leading.equalTo(mapView.snp.leading).offset(20)
+            make.bottom.equalTo(mapView.snp.bottom).inset(40)
         }
     }
 }
